b58949bf59d7dd7487e68600b4436102f36fbf26
RexAttaque <tristanmontagnier@gmail.com>
2023-04-10 19:00:17 +0200

    Added GSM module library

---
+++ include/GSM/Sim800L.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <Arduino.h>
+#include <faultCodes.h>
+
+boolean debug=true; 
+boolean cli_interface=false; 
+
+const int signalQuality_floor = 10; // goes from 0 to 31 with 31 being excellent
+
+class GSM_obj {
+    private :
+
+        HardwareSerial GSM_Serial;
+
+        bool _debug; //debug indicator, outputs via Serial
+        bool cli_interface; //command line interface with the module
+
+        String phoneNumber = ""; //phone number of the inserted SIM
+        String RX = ""; //last transmission from the module
+        String TX = ""; //last message sent to the module
+        //String PIN = "0000"; //remove the PIN from the inserted SIM card before use
+
+    public :
+
+        GSM_obj(HardwareSerial HWSerial, String GSM_num, bool debug, bool cli);
+
+        String getLastTX();
+        
+        void setPhoneNumber(String GSM_num);
+        void setTX(String message);
+        //void setPIN();
+
+        bool check_SIM_GSM(); //Checks wether a SIM is present or not
+        bool check_REG_GSM(); //Checks wether or not the module has registered on a network
+        bool check_SIG_GSM(int qualityFloor); //Checks the reported signal quality, returns false if it is below the floor
+        
+        void read_RX(); //reads the incoming module transmission and stores it in RX
+        bool check_RX_OK(); //Checks that the last two characters of RX are OK
+        bool check_AT_OK_GSM(char c); //Sends then checks that the module has received the character c
+        bool check_AT_OK_GSM(String AT_COMMAND); //Sends then checks that the module has received the command AT_COMMAND
+        void TRX_AT_GSM(char c); //Sends the charater c to the module and listens for the answer that is stored in RX
+        void TRX_AT_GSM(String AT_COMMAND); //Sends the command AT_COMMAND to the module and listens for the answer that is stored in RX
+
+        void updateSerial(); //Allows for direct communication to the module if cli_interface = true via the serial monitor
+
+        bool init(); //initializes the module (checks SIM, REG and SIG), returns false if no SIM, no REG or low SIG quality
+        bool PrepSend_s1(); //Sending an SMS is cut into multiple stages because the module is too slow to execute everything at once
+        bool PrepSend_s2();
+        bool PrepSend_s3();
+        bool sendSMS(); //Sends the TX string via SMS
+};
---
+++ lib/GSM/Sim800L.cpp
@@ -0,0 +1,216 @@
+#include <GSM/Sim800L.h>
+
+// Check wether the SIM is detected or not
+GSM_obj::GSM_obj(HardwareSerial HWSerial, String GSM_num, bool debug, bool cli)
+:GSM_Serial(HWSerial),phoneNumber(GSM_num),_debug(debug),cli_interface(cli)
+{}
+
+String GSM_obj::getLastTX()
+{
+    return TX;
+}
+
+void GSM_obj::setPhoneNumber(String GSM_num)
+{
+    phoneNumber = GSM_num;
+}
+
+void GSM_obj::setTX(String message)
+{
+    TX = message;
+}
+
+bool GSM_obj::check_SIM_GSM()
+{
+  int ICCID_start_pos = 10;
+
+  //check if the ICCID end with 89 which should always be the case
+  if(check_AT_OK_GSM("AT+CCID") && RX.substring(ICCID_start_pos, ICCID_start_pos+2) == "89")
+  {
+    //code to fetch only the whole ICCID
+    if(_debug) 
+    {
+        int i=0;
+        while(RX.charAt(ICCID_start_pos+i) != char(13)) //char(13) is the return carriage character
+        {
+        i++;
+        }
+        Serial.println("ICCID : " + RX.substring(ICCID_start_pos,ICCID_start_pos+i)); // Print ICCID
+    }
+    
+    return true;
+  }
+
+  return false;
+}
+
+// Check whether the module has registered on the network
+bool GSM_obj::check_REG_GSM()
+{
+  int REG_status_pos = 20;
+
+  if(check_AT_OK_GSM("AT+CREG?") && RX.charAt(REG_status_pos) == char(49)) //char(49) is "1"
+  { 
+    return true;
+  }
+
+  return false;
+}
+
+// Signal quality test, value range is 0 to 31 with 31 being excellent. Returns true of signal quality is above the floor, false otherwise
+bool GSM_obj::check_SIG_GSM(int qualityFloor)
+{
+  int QL_start_pos = 15;
+
+  if(check_AT_OK_GSM("AT+CSQ"))
+  {
+    int i=0;
+    while(RX.charAt(QL_start_pos+i) != char(44)) //char(44) is ","
+    {
+      i++;
+    }
+  
+    int signalQuality = RX.substring(QL_start_pos, QL_start_pos+i).toInt();
+  
+    if(signalQuality != 99 && signalQuality >= qualityFloor)
+    {  
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void GSM_obj::read_RX()
+{
+  RX = Serial3.readString();
+  if(_debug) Serial.println(RX); 
+}
+
+// Function to check wether the last two characters of an answer are "OK" indicating the receiver answered and understood, returns the RX if OK is found, returns an empty string otherwise
+bool GSM_obj::check_RX_OK()
+{
+    int RX_length = RX.length();
+    //check the substring before the /r/n characters for "OK"
+    if(RX.substring(RX_length-4, RX_length-2) == "OK")
+    {
+        if(_debug) Serial.println("PASS\n");
+
+        return true;
+    }
+
+    if(_debug) Serial.println("FAIL\n");
+    RX="";
+    return false;
+}
+
+bool GSM_obj::check_AT_OK_GSM(char c)
+{
+    TRX_AT_GSM(c);
+    return check_RX_OK();
+}
+
+bool GSM_obj::check_AT_OK_GSM(String AT_COMMAND)
+{ 
+    TRX_AT_GSM(AT_COMMAND);
+    return check_RX_OK();
+}
+
+void GSM_obj::TRX_AT_GSM(char c)
+{
+    Serial3.write(c);
+    read_RX();
+}
+
+// Function to send an AT command to the SIM800L module, returns the answer
+void GSM_obj::TRX_AT_GSM(String AT_COMMAND) 
+{
+    Serial3.println(AT_COMMAND);
+    read_RX();
+}
+
+
+
+bool GSM_obj::init()
+{
+  // Send attention command to check if all fine, it returns OK
+  check_AT_OK_GSM("AT");
+  
+  if(RX!="" && check_SIM_GSM())
+  {
+    // Wait for network registration
+    while(check_REG_GSM() == false)
+    {
+      delay(1000);
+    }
+
+    if(!check_SIG_GSM(signalQuality_floor)) 
+    {
+      if(_debug) Serial.println("WARNING, SIGNAL QUALITY LOW, CHECK DEBUG");
+    }
+
+    return true;
+  }
+  else if(_debug)
+  {
+    Serial.println("ERROR, INIT FAIL, CHECK DEBUG, EXIT");
+  }
+
+  return false;
+}
+
+bool GSM_obj::PrepSend_s1()
+{
+  // Starts SMS mode
+  return check_AT_OK_GSM("AT+CMGF=1"); //if check_AT_OK_GSM doesn't work, simply use Serial3.print
+}
+
+bool GSM_obj::PrepSend_s2()
+{
+  // Receiving phone number
+  return check_AT_OK_GSM("AT+CMGS=\"" + phoneNumber + "\"\r");
+}
+
+bool GSM_obj::PrepSend_s3()
+{
+  // Message
+  return check_AT_OK_GSM(TX);
+}
+
+
+bool GSM_obj::sendSMS()
+{
+  if(_debug) Serial.println("Sending text message...");
+  
+  // CTR+Z in ASCII, indicates the end of the message
+  bool status = check_AT_OK_GSM(26); //check_AT_OK_GSM didn't work previously, use check_AT_OK_GSM("")
+  
+  while(Serial3.available()) {
+    if(_debug)
+    {
+      Serial.write(Serial3.read()); //Clear the contents of the sim800L TX buffer and display them in the console for debugging
+    }
+    else
+    {
+      Serial3.read(); //Clear the contents of the sim800L TX buffer
+    }
+  }
+  
+  if(_debug) Serial.println("\nText sent");
+
+  return status;
+}
+
+
+// For data transmission from Serial3 to Serial port & vice versa, debug only
+void GSM_obj::updateSerial()
+{
+  while(Serial3.available()) {
+    Serial.write(Serial3.read());//Forward what Serial3 received to Serial Port
+  }
+  Serial.println("");
+  
+  while(Serial.available()) {
+    Serial3.write(Serial.read());//Forward what Serial received to Serial2 Port
+  }
+}
