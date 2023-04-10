dee1b86e633d632d24ce1d0d0c775e53a64b8ed1
RexAttaque <tristanmontagnier@gmail.com>
2023-04-10 16:37:17 +0200

    Added GPS default refresh rate

--- include/Sensors/GPS_UBX9/ModuleLibs/GPS_UBX9.h
+++ include/Sensors/GPS_UBX9/ModuleLibs/GPS_UBX9.h
@@ -3,10 +3,11 @@
 #include <Sensors/Base_Sensor.h>
 #include <Sensors/GPS_UBX9/InterfaceLibs/ubloxGen9.h>
 
-GPS_UBX9 GPS1_Avio = GPS_UBX9(Serial1);
+GPS_UBX9 GPS1_Avio = GPS_UBX9();
 const uint8_t qty_GPS_Avio = 1;
 GPS_UBX9* GPS_Avio_array;
 
+const uint16_t default_GPS_UBX9_Hz = 25; //default GPS refresh rate (Hz)
 const uint8_t GPS_varAmount = 5; //time,x,y,z,CEP (ms ; cm)
 
 class GPS_UBX9 : public Base_Sensor<long> {
@@ -15,7 +16,7 @@ class GPS_UBX9 : public Base_Sensor<long> {
   
   public :
   
-    GPS_UBX9(HardwareSerial HWSerial);
+    GPS_UBX9(HardwareSerial HWSerial = Serial1, long Baud = 115200, uint8_t Pltfrm_Model = 8, uint16_t refreshRate = default_GPS_UBX9_Hz, uint8_t Nav_Rate = 1, uint8_t config_Level = 7, long default_Baud = 38400, bool buffered_POS = false, bool NMEA_USB = false, long USB_Baud = 115200, bool NMEA_UART1 = false, uint8_t Stop_Bits = 1);
 
     bool init();
 
--- lib/Sensors/GPS_UBX9/ModuleLibs/GPS_UBX9.cpp
+++ lib/Sensors/GPS_UBX9/ModuleLibs/GPS_UBX9.cpp
@@ -1,6 +1,7 @@
 #include <Sensors/GPS_UBX9/ModuleLibs/GPS_UBX9.h>
 
-GPS_UBX9::GPS_UBX9(HardwareSerial HWSerial):Base_Sensor(GPS_varAmount),GPS(ublox_gen9(HWSerial))
+GPS_UBX9::GPS_UBX9(HardwareSerial HWSerial, long Baud, uint8_t Pltfrm_Model, uint16_t refreshRate, uint8_t Nav_Rate, uint8_t config_Level, long default_Baud, bool buffered_POS, bool NMEA_USB, long USB_Baud, bool NMEA_UART1, uint8_t Stop_Bits)
+:Base_Sensor(GPS_varAmount, refreshRate),GPS(ublox_gen9(HWSerial, Baud, Pltfrm_Model, (uint16_t) 1000/refreshRate, Nav_Rate, config_Level, default_Baud, buffered_POS, NMEA_USB, USB_Baud, NMEA_UART1, Stop_Bits))
 {}
 
 bool GPS_UBX9::init()
