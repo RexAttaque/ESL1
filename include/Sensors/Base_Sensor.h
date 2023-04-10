8c2353ffdf8eaa7e17210f179ae5c34426ab7e94
RexAttaque <tristanmontagnier@gmail.com>
2023-04-10 16:25:26 +0200

    Added refreshRate to Base_Sensor and virtual methods to go live and go idle (meant to save power)

--- include/Sensors/Base_Sensor.h
+++ include/Sensors/Base_Sensor.h
@@ -8,18 +8,22 @@ class Base_Sensor {
     protected :
         T* meas;
         uint8_t _varAmount;
+        uint16_t _refreshHz;
 
         void allocateMemory();
         void freeMeasMemory();
         void reallocateMemory();
 
     public :
-        Base_Sensor(uint8_t varAmount);
+        Base_Sensor(uint8_t varAmount, uint16_t refreshHz);
 
         virtual bool init();
         virtual uint8_t getStatus();
         virtual uint8_t getCalibration();
         virtual T* getMeas();
+
+        virtual bool goLive();
+        virtual bool goIdle();
 
         uint8_t get_var_amount();
 };
--- lib/Sensors/Base_Sensor.cpp
+++ lib/Sensors/Base_Sensor.cpp
@@ -1,7 +1,8 @@
 #include <Sensors/Base_Sensor.h>
 
 template <class T>
-Base_Sensor<T>::Base_Sensor(uint8_t varAmount):_varAmount(varAmount)
+Base_Sensor<T>::Base_Sensor(uint8_t varAmount, uint16_t refreshHz)
+:_varAmount(varAmount),_refreshHz(refreshHz)
 {
     allocateMemory();
 }
@@ -30,6 +31,19 @@ T* Base_Sensor<T>::getMeas()
 {
     return nullptr;
 }
+
+template <class T>
+bool Base_Sensor<T>::goLive()
+{
+    return false;
+}
+
+template <class T>
+bool Base_Sensor<T>::goIdle()
+{
+    return false;
+}
+
 
 //Actual common methods
 template <class T>
