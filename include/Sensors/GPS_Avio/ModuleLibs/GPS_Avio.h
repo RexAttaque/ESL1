4ba5f8b21e23033faf751f940227a6eba69bc8d8
RexAttaque <tristanmontagnier@gmail.com>
2023-04-10 11:54:54 +0200

    Fixed GPS getMeas (was the wrong size) and added GPS status method

--- include/Sensors/GPS_Avio/ModuleLibs/GPS_Avio.h
+++ include/Sensors/GPS_Avio/ModuleLibs/GPS_Avio.h
@@ -7,7 +7,7 @@ GPS_Avio GPS1_Avio = GPS_Avio(Serial1);
 const uint8_t qty_GPS_Avio = 1;
 GPS_Avio* GPS_Avio_array;
 
-const uint8_t GPS_varAmount = 3; //x,y,z (cm)
+const uint8_t GPS_varAmount = 5; //time,x,y,z,CEP (ms ; cm)
 
 class GPS_Avio : public Base_Sensor<long> {
   private :
@@ -16,6 +16,8 @@ class GPS_Avio : public Base_Sensor<long> {
   public :
   
     GPS_Avio(HardwareSerial HWSerial);
+
+    uint8_t getStatus();
 
     long* getMeas();
 };
--- lib/Sensors/GPS_Avio/ModuleLibs/GPS_Avio.cpp
+++ lib/Sensors/GPS_Avio/ModuleLibs/GPS_Avio.cpp
@@ -3,6 +3,11 @@
 GPS_Avio::GPS_Avio(HardwareSerial HWSerial):Base_Sensor(GPS_varAmount),GPS(ublox_gen9(HWSerial))
 {}
 
+uint8_t GPS_Avio::getStatus()
+{
+    return GPS.getNavFixStatus();
+}
+
 long* GPS_Avio::getMeas()
 {
   return GPS.getPOSECEF();
