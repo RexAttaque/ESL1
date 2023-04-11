98203ecd62a22ddf451701d6a18d5c6bf6c1af80
RexAttaque <tristanmontagnier@gmail.com>
2023-04-12 01:49:00 +0200

    Added logging in init for BMP280, Made sure to prepare the measures consecutively

--- lib/Sensors/BARO_BMP280/ModuleLibs/BARO_BMP280.cpp
+++ lib/Sensors/BARO_BMP280/ModuleLibs/BARO_BMP280.cpp
@@ -6,14 +6,33 @@ BARO_BMP280::BARO_BMP280(uint8_t address, Adafruit_BMP280::sensor_sampling over_
     
 bool BARO_BMP280::init()
 {
-  bool status = BARO.begin(_address);
+  bool status = BARO.begin(_address); //begin already checks for correct detection of the sensor and I2C start
 
   if(status)
   {
+    if(debug::info()) 
+    {
+      debug::Serial.println("      --->BMP280 Active and available on I2C...");
+      debug::Serial.println("      --->Setting sampling mode...");
+    }
+
     BARO.setSampling(_mode,_over_samp_T,_over_samp_P,_filter,_stb_time); 
+    if(_mode == Adafruit_BMP280::MODE_FORCED) 
+    {
+      if(debug::info()) debug::Serial.println("      --->BMP280 in force mode, taking first measurement...");
+      BARO.takeForcedMeasurement(); //take a first measurement to sort of kick start the sensor if it's in Force mode, otherwise it'll do it on it's own
+    }  
+    else
+    {
+      if(debug::info()) debug::Serial.println("      --->BMP280 in normal mode, letting it take it's first measurement...");
+      delay(5000); //Give the sensor time, whatever the parameters, to take at least one measurement
+    }
+    //check that the first measurement is valid ?
+
+    return true;
   }
 
-  return status;
+  return false;
 }
 
 float* BARO_BMP280::getMeas()
@@ -22,6 +41,11 @@ float* BARO_BMP280::getMeas()
 
   meas[1] = BARO.readPressure();
   meas[2] = BARO.readTemperature();
+
+  if(_mode == Adafruit_BMP280::MODE_FORCED) 
+  {
+    BARO.takeForcedMeasurement(true); //Start the measurement process for next time, skip delays inside takeForceMeasurements as this is running inside a tightly timed loop
+  }  
 
   return meas;
 }
