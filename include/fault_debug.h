48889170e0c9325973cd0cae668e23d5249c4e68
RexAttaque <tristanmontagnier@gmail.com>
2023-07-22 18:56:50 +0200

    fault debug revamp

--- include/fault_debug.h
+++ include/fault_debug.h
@@ -2,42 +2,54 @@
 
 #include <Arduino.h>
 
-namespace debug {
-    bool _full = false;
-    bool _trace = false;
-    bool _info = false;
-
-    usb_serial_class Serial_USB = Serial;
-    //HardwareSerial Serial = SerialX; //if a hardware serial wants to be used for debug, to send to telem or something
-
-    bool full()
-    {
-        return _full;
-    }
-
-    bool trace()
-    {
-        return (_full || _trace);
-    }
-
-    bool info()
-    {
-        return (_full || _trace || _info);
-    }
-
-    void disable()
-    {
-        //if(debug::info()) debug::Serial_USB.println("Good night ESO, see you after the flight... TM"); //To re-enable last minute
-        _full = false;
-        _trace = false;
-        _info = false;
-        debug::Serial_USB.end();
-    }
-}
-
 namespace faultCodes {
     enum faultCodes {
         pos = 0,
         altitude = -9999, //if this altitude is thrown out, whatever altitude measurement method failed
     };
 }
+
+namespace debugLevel {
+    const uint8_t FULL = 3; //All debug logs
+    const uint8_t TRACE = 2; //All debug logs that allow to track progress
+    const uint8_t INFO = 1; //Only information
+    const uint8_t NONE = 0; //Nothing
+
+    const String levelName[FULL] = {"INFO", "TRACE", "FULL"}; //Name of the debug levels for display
+}
+
+
+
+namespace debug {
+    usb_serial_class Serial_USB = Serial; //USB serial channel ("Serial" var by def), ! type changes on different duino like boards !
+    unsigned long USB_baud = 115200; //USB baud rate (115200 by def)
+
+    HardwareSerial def_Serial_Hard = Serial7; //Default serial channel for debug ("Serial7" var by def)
+    unsigned long def_SerialHard_baud = 115200; //Default hardware serial channel baud for debug (115200 by def)
+}
+
+
+
+class fault_debug
+{
+private:
+    bool chan_rdy = false;
+
+    bool HW_chan = false;
+    HardwareSerial Serial_HW; //if a hardware serial needs to be used for debug, to send to telem or something
+    unsigned long HW_baud;
+
+    String ID; //ID of the debug instance
+    String _IDsub = ""; //Set sub ID that will be printed along with the ID, can be changed
+    uint8_t l; //Debug level of the debug instance (the higher the level the more is displayed, see debugLevel namespace for levels and comments)
+public:
+    fault_debug(String module_ID, uint8_t level = debugLevel::FULL, HardwareSerial hard_chan = debug::def_Serial_Hard);
+
+    bool begin(bool enableHW_chan = false, unsigned long hard_baud = debug::def_SerialHard_baud);
+    void set_HWbaud(unsigned long baud_hard);
+    void end(bool closeHW_chan = false);
+
+    void set_subID(String sub_ID);
+    void print(uint8_t level, String msg, String sub_ID = "");
+    void println(uint8_t level, String msg, String sub_ID = "");
+};
---
+++ src/fault_debug.cpp
@@ -0,0 +1,78 @@
+#include <fault_debug.h>
+
+//Initializes debug instance with full debug and Serial7 hardware channel by default, ID must be specified (and for clarity, should be unique)
+fault_debug::fault_debug(String module_ID, uint8_t level, HardwareSerial hard_chan):ID(module_ID),l(level),Serial_HW(hard_chan)
+{}
+
+//Starts serial ports (Serial_USB or Serial_HW (at 115200 by def)). The use of the hardware channel is not mandatory but replaces the USB channel. 
+bool fault_debug::begin(bool enableHW_chan, unsigned long baud_hard)
+{
+    if(!enableHW_chan && !debug::Serial_USB)
+    {
+        debug::Serial_USB.begin(debug::USB_baud);
+    }
+
+    if(enableHW_chan && !Serial_HW)
+    {
+        set_HWbaud(baud_hard);
+        Serial_HW.begin(HW_baud);
+    }
+
+    if((!enableHW_chan && debug::Serial_USB) || (enableHW_chan && Serial_HW))
+    {
+        chan_rdy = true;
+        HW_chan = enableHW_chan;
+        println(debugLevel::INFO, "Debug Initialized", "fault_debug::begin");
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void fault_debug::set_HWbaud(unsigned long baud_hard)
+{
+    HW_baud = baud_hard;
+}
+
+//Simply sets the debug level to 0 (no output), none of the Serial ports are closed to avoid conflicts unless specified for HW channel
+void fault_debug::end(bool closeHW_chan)
+{
+    l = debugLevel::NONE;
+
+    if(closeHW_chan)
+    {
+        Serial_HW.end();
+    }
+}
+
+void fault_debug::set_subID(String subID)
+{
+    _IDsub = subID;
+}
+
+// Prints a line without going to the next, msg structure is "ID | subID | debugLevelName | msg", subID is by default "" but is stored in memory if specified once, use set_subID if you only want to change the subID without printing
+void fault_debug::print(uint8_t level, String msg, String sub_ID)
+{
+    if(chan_rdy && l>=level && level != 0)
+    {
+        if(sub_ID != "") set_subID(sub_ID);
+        String compl_msg = ID + " | " + _IDsub + " | " + debugLevel::levelName[level-1] + " | " + msg;
+        
+        if(HW_chan && Serial_HW)
+        {
+            Serial_HW.print(compl_msg);
+        }
+        else if(debug::Serial_USB && !HW_chan)
+        {
+            debug::Serial_USB.print(compl_msg);
+        }
+    }
+}
+
+// Prints a line and goes to the next, see fault_debug::print()
+void fault_debug::println(uint8_t level, String msg, String sub_ID)
+{
+    print(level, msg + "\r\n", sub_ID);
+}
