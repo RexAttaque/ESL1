88f03ef544305eac7eff092d8e7eb63ebc1bb291
RexAttaque <tristanmontagnier@gmail.com>
2023-07-23 17:08:37 +0200

    TELEM_obj constr default args to TELEM_const

--- include/TELEM/ModuleLibs/SX1267MB1MAS.h
+++ include/TELEM/ModuleLibs/SX1267MB1MAS.h
@@ -4,12 +4,15 @@
 #include <fault_debug.h>
 
 namespace TELEM_const {
-    const uint8_t PacketSize = 38;
-    // const unsigned long FREQ = 433E6;   // Frequency band (This unit is optimized for 433MHz (Low Freq. / LF) and 868MHz (High Freq. / HF))
-    // const int8_t PWR = 20;              // Power (0-20)
-    // const uint8_t SF = 9;               // Spreading factor (6-12)
-    // const unsigned long BW = 250E3;     // Bandwidth (10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125, 250, 500 kHz)
-    // const uint8_t CR = 8;               // Coding rate (5 -> 4/5 (1.25 overhead), 6 -> 4/6 (1.5), 7 -> 4/7 (1.75), 9 -> 4/8 (2))
+    const uint8_t PacketSize = 38;      // Size of the packets to transmit (number of )
+
+    const unsigned long FREQ = 433E6;   // Frequency band (This unit is optimized for 433MHz (Low Freq. / LF) and 868MHz (High Freq. / HF))
+    const int8_t PWR = 20;              // Power (0-20)
+    const uint8_t SF = 9;               // Spreading factor (6-12)
+    const unsigned long BW = 250E3;     // Bandwidth (10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125, 250, 500 kHz)
+    const uint8_t CR = 8;               // Coding rate (5 -> 4/5 (1.25 overhead), 6 -> 4/6 (1.5), 7 -> 4/7 (1.75), 9 -> 4/8 (2))
+
+    const long HW_baud = 115200;
 }
 
 struct TELEM_packet {
@@ -25,7 +28,7 @@ class TELEM_obj {
 
     public :
 
-        TELEM_obj(HardwareSerial HWSerial = Serial3, long Baud = 115200, unsigned long freq = 433E6, int8_t power = 20, uint8_t spreadfactor = 9, unsigned long bandwidth = 250E3, uint8_t codingRate = 8); 
+        TELEM_obj(HardwareSerial HWSerial = Serial3, long Baud = TELEM_const::HW_baud, unsigned long freq = TELEM_const::FREQ, int8_t power = TELEM_const::PWR, uint8_t spreadfactor = TELEM_const::SF, unsigned long bandwidth = TELEM_const::BW, uint8_t codingRate = TELEM_const::CR); 
 
         bool setFreq();
         bool setPwr();
